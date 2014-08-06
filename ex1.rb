4247718c0bc3f6da596be6f32505ef9dbf4bd931
chrisjohnson89 <afckazoo@gmail.com>
2014-08-06 16:26:42 -0700

    First Commit for LRTHW

---
+++ ex1.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+puts "Hello Again."
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd must rather you 'not'." 
+puts #'I "said" do not touch this.'
+
+#Make your script print another line.
+#Make your script print only one of the lines.
+#Put a '#' (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
