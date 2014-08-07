38d1ad416d26d7ebcce2537602a95e6b453f7dec
chrisjohnson89 <afckazoo@gmail.com>
2014-08-07 13:40:27 -0700

    ex4 completed

--- ex4.rb
+++ ex4.rb
@@ -1,11 +1,19 @@
 
+# cars equals 100
 cars = 100
+# space_in_car equals 4.0(floating point)
 space_in_a_car = 4.0
+# drivers equals 30
 drivers = 30 
+# passengers equals 90
 passengers = 90
+# cars_not_driven equals cars(100) - driver(30) 
 cars_not_driven = cars - drivers
+# cars_driven equals drivers(30)
 cars_driven = drivers
+# carpool_capacity equals cars_driven(drivers(30)) times space_in_a_car(4.0)
 carpool_capacity = cars_driven * space_in_a_car
+# average_passengers_per_car equals passengers(90) divided by cars_driven(drivers(30))
 average_passengers_per_car = passengers / cars_driven
 
 
