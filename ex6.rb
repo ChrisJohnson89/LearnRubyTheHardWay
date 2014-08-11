7195801b826c35f8b2ec116469a3e71be24a05d2
chrisjohnson89 <afckazoo@gmail.com>
2014-08-11 13:51:30 -0700

    completed ex6

---
+++ ex6.rb
@@ -0,0 +1,30 @@
+# types_of_people equals 10  
+types_of_people = 10
+# x equals a string with the varible types_of_people
+x = "There at #{types_of_people} types of people"
+# binary equals "binary"
+binary = "binary"
+# do_not equals "don't"
+do_not = "don't"
+# y eqauls a string with the varibles "binary" and "do_not"
+y = "Those who know #{binary} and those who #{do_not}."
+# displays x, x = "There at 10 types of people"
+puts x
+# displays y, y = "Those who know binary and those who don't."
+puts y
+# displays , "I said: There at 10 types of people"
+puts "I said: #{x}."
+# displays , "I also said: Those who know binary and those who don't."
+puts "I also said: '#{y}'."
+# hilarious equals false 
+hilarious = false
+# joke_evaluation equals a string with the varible hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+# displays , "Isn't that joke so funny?! false"
+puts joke_evaluation
+# w equals "This is the left side of..."
+w = "This is the left side of..."
+# e equals " a string with a right side."
+e = ' a string with a right side.'
+# displays "This is the left side of...a string with a right side."
+puts w + e
